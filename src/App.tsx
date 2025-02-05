2b6eea039f034be8aa1a79c5a07abd61c496adb1
koldun <anton.kitaev.97@gmail.com>
2025-02-05 02:05:52 -0800

    add: constant PATHES

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from '@/components/Header/index'
-import {
-    HEADER_HOME_PATH,
-    HEADER_SETTINGS_PATH,
-} from '@/constants/textConstants'
+import { PATHES } from '@/constants/textConstants'
 
 import { GlobalStyle } from './GlobalStyle'
 
@@ -16,9 +13,9 @@ function App() {
             <Router>
                 <Header />
                 <Routes>
-                    <Route path={HEADER_HOME_PATH} element={<div>Home</div>} />
+                    <Route path={PATHES.home} element={<div>Home</div>} />
                     <Route
-                        path={HEADER_SETTINGS_PATH}
+                        path={PATHES.settings}
                         element={<div>Settings</div>}
                     />
                     <Route path="*" element={<div>Not Found</div>} />
