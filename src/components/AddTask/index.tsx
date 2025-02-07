9c77da89e958c2746e19f986f0fb21725d2a82c3
koldun <anton.kitaev.97@gmail.com>
2025-02-07 15:41:45 +0300

    add: use state ti add task input

--- src/components/AddTask/index.tsx
+++ src/components/AddTask/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as S from './styled'
 
 export default function AddTask() {
+    const [taskName, setTaskName] = useState('')
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskName(e.target.value)
+    }
     return (
         <S.StyledSection>
             <S.StyledLabel>
                 <S.StyledLegend>Add a new task</S.StyledLegend>
-                <S.StyledDisplay placeholder="input your task"></S.StyledDisplay>
+                <S.StyledDisplay
+                    value={taskName}
+                    placeholder="input your task"
+                    onInput={handleInput}
+                ></S.StyledDisplay>
             </S.StyledLabel>
             <S.StyledAddButton type="button">{'Add todo'}</S.StyledAddButton>
         </S.StyledSection>
